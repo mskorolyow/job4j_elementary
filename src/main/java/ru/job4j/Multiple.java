c87e768a8fb07d350b4f87b3bead2a6d456996ac
kms <do.88000@gmail.com>
2025-01-17 15:28:27 +0300

    1.3 Задание Разбор первой программы по "взрослому" [#505151]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+public class Multiple {
+
+    public static void main(String[] args) {
+
+        int j = 1;
+
+        System.out.println("Таблица умножения на 1: ");
+
+        for(int i=1;i<=10;i++) {
+
+            System.out.println( j + " * " + i + " = " + i*j + ";" );
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class StudentInfo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Maxim Korolyow");
+        System.out.println("Born in 08.05.1986");
+
+    }
+}
