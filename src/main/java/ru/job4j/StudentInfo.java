751c6fcdb2a0f2aa1a326a86c92b7a82b8a9bf01
kms <do.88000@gmail.com>
2025-01-17 15:32:04 +0300

    1.3 Задание Разбор первой программы по "взрослому" [#505151]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -5,7 +5,7 @@ public class StudentInfo {
     public static void main(String[] args) {
 
         System.out.println("Maxim Korolyow");
-        System.out.println("Born in 08.05.1986");
+        System.out.println("08.05.1986");
 
     }
 }
