e27c97514892ee9dbecc31adc02805f4e2c5e055
kms <do.88000@gmail.com>
2025-01-22 10:31:38 +0300

    1.1. Что такое import [#505278] исправил!!!

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -3,7 +3,6 @@ package ru.job4j.calculator;
 import ru.job4j.math.*;
 
 public class MathCalculator {
-
     public static double sumAndMultiply(double first, double second) {
 
         return MathFunction.sum(first, second) + MathFunction.multiply(first, second);
