8430879a3c4db49fa19774aaf908735ecccdbcad
kms <do.88000@gmail.com>
2025-01-22 15:39:53 +0300

    2. Тесты для задачи "идеальный вес" [#505260] Исправил проблему.

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -4,6 +4,7 @@ public class Converter {
     public static double rubleToEuro(double value) {
         return value / 90;
     }
+
     public static double rubleToDollar(double value) {
         return value / 60;
     }
