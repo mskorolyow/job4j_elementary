401ccf66eb7ef27d58dacd9d82340ae973d7a1fe
kms <do.88000@gmail.com>
2025-01-23 09:26:13 +0300

    1. Модульные тесты [#505277 #529567]

--- src/test/java/ru/job4j/ConverterTest.java
+++ src/test/java/ru/job4j/ConverterTest.java
@@ -1,11 +1,10 @@
 package ru.job4j;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.junit.jupiter.api.Test;import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class ConverterTest {
+
     @Test
     void whenConvert140RblThen2Euro() {
         float input = 140;
