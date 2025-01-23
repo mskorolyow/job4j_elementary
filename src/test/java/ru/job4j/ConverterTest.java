e7d65f67c7f19ab23f07097f68a987b81dbdef2d
kms <do.88000@gmail.com>
2025-01-23 08:29:13 +0300

    2. Тесты для задачи "идеальный вес" [#505260 #529593] исправил!

--- src/test/java/ru/job4j/ConverterTest.java
+++ src/test/java/ru/job4j/ConverterTest.java
@@ -1,10 +1,8 @@
 package ru.job4j;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.BDDAssertions.withPrecision;
-
 class ConverterTest {
     @Test
     void whenConvert140RblThen2Euro() {
@@ -12,15 +10,14 @@ class ConverterTest {
         float expected = 2;
         float output = (float) Converter.rubleToEuro(input);
         float value = 0.0001f;
-        assertThat(output).isEqualTo(expected, withPrecision(value));
+        assertThat(output).isEqualTo(expected, Assertions.withPrecision(value));
     }
-
     @Test
     void whenConvert200RbThen3dot3333Euro() {
         float input = 180;
         float expected = 3;
         float output = (float) Converter.rubleToDollar(input);
         float value = 0.0001f;
-        assertThat(output).isEqualTo(expected, withPrecision(value));
+        assertThat(output).isEqualTo(expected, Assertions.withPrecision(value));
     }
 }
