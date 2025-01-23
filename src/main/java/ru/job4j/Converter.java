cfa68cff8aac0b40b32770e6da89c706bc77bacf
kms <do.88000@gmail.com>
2025-01-23 09:24:35 +0300

    1. Модульные тесты [#505277 #529567]

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -4,7 +4,6 @@ public class Converter {
     public static double rubleToEuro(double value) {
         return value / 90;
     }
-
     public static double rubleToDollar(double value) {
         return value / 60;
     }
