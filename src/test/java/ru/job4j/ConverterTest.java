88a486ae5183fae1e65185cd79143e99f8c742db
kms <do.88000@gmail.com>
2025-01-23 09:35:49 +0300

    1. Модульные тесты [#505277 #529567] !!! Добавил пробел перед строкой 7

--- src/test/java/ru/job4j/ConverterTest.java
+++ src/test/java/ru/job4j/ConverterTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class ConverterTest {
 
     @Test
