54c64e4665bff0ccc7e2ab60538638ceb4eb6e31
kms <do.88000@gmail.com>
2025-01-23 09:44:10 +0300

    shit

--- src/test/java/ru/job4j/ConverterTest.java
+++ src/test/java/ru/job4j/ConverterTest.java
@@ -3,10 +3,7 @@ package ru.job4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-
 class ConverterTest {
-
     @Test
     void whenConvert140RblThen2Euro() {
         float input = 140;
@@ -15,7 +12,6 @@ class ConverterTest {
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, Assertions.withPrecision(value));
     }
-
     @Test
     void whenConvert180RbThen3Euro() {
         float input = 180;
