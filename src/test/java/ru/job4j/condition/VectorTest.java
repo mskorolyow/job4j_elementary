ea136821491d83a2478559824e0eb0f715264c7f
kms <do.88000@gmail.com>
2025-01-23 15:43:44 +0300

    Тестирование Класса Вычисления Диагонали Прямоугольника [#505281]

--- src/test/java/ru/job4j/condition/VectorTest.java
+++ src/test/java/ru/job4j/condition/VectorTest.java
@@ -16,7 +16,7 @@ class VectorTest {
 
     @Test
     void whenVectorsMinus12And34ThenResult22() {
-        int x1 = -1, y1 = -2, x2 = 3, y2 = 4;
+        int x1 =  -1, y1 = -2, x2 = 3, y2 = 4;
         String expected = "(2, 2)";
         String output = Vector.addVectors(x1, y1, x2, y2);
         assertThat(output).isEqualTo(expected);
