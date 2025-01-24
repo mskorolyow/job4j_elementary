621d887f9d087d89e10386acfcc835e0f8c15401
kms <do.88000@gmail.com>
2025-01-24 14:35:21 +0300

    Тестирование Класса Калькулятора Температуры Хранения [#505279 #529605] Убрал пустые строки внутри методов.

--- src/test/java/ru/job4j/calculator/TemperatureFitTest.java
+++ src/test/java/ru/job4j/calculator/TemperatureFitTest.java
@@ -10,21 +10,17 @@ class TemperatureFitTest {
 
     @Test
     void whenRoomTemperature10ThenFruits8() {
-
         int input = 10;
         double expected = 8.0;
         double output = TemperatureFit.idealTemperatureForFruits(input);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
-
     }
 
     @Test
     void whenRoomTemperature10ThenMeat5() {
-
         int input = 10;
         double expected = 5.0;
         double output = TemperatureFit.idealTemperatureForMeat(input);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
-
     }
 }
