9a932e3389cf6d5b5f099a6ecf54e1bab6e19aca
kms <do.88000@gmail.com>
2025-01-29 08:06:01 +0300

    Простое число (оператор прерывания break) [#505214 #530370] Убрал if по рекомендации.

--- src/main/java/ru/job4j/loop/PrimeNumber.java
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -3,14 +3,11 @@ package ru.job4j.loop;
 public class PrimeNumber {
     public static int calc(int finish) {
         int count = 0;
-        if (finish > 1) {
             for (int i = 2; i <= finish; i++) {
-
                 if (CheckPrimeNumber.check(i)) {
                     count++;
                 }
             }
-        }
         return count;
     }
 }
