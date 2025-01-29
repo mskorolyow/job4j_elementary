4b0197d41089ebf3bb626e1e94f72b3c133a5f36
kms <do.88000@gmail.com>
2025-01-29 08:08:43 +0300

    Исправил.

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,14 +2,15 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean result = false;
+        if (number == 1) {
+            return false;
+        }
+        boolean result = true;
         for (int i = 2; i <= number - 1; i++) {
             if (number % i == 0) {
                 result = false;
                 break;
-            } else {
-                result = true;
-            }
+           }
         }
         return result;
     }
