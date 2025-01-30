0d0fe10ac1860a80f6b1611c37f3003df8d38835
kms <do.88000@gmail.com>
2025-01-30 10:22:46 +0300

    Размер массива [#505221]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,12 +5,15 @@ package ru.job4j.array;
  *    а) Java 8 (или 9), Полное руководство - Герберт Шилдт. Глава 3. Массивы (стр. 94 - 101)
  *    б) Библиотека профессионала. Java. Том 1. Основы - Кей Хостманн, Гари Корнелл.  Глава 3. Массивы (стр. 116 - 129)
  *            Страницы примерные.
- *
  */
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        System.out.println("Размер массива ages равен: " + ages.length);
+        System.out.println("Размер массива surnames равен: " + surnames.length);
+        System.out.println("Размер массива prices равен: " + prices.length);
     }
 }
