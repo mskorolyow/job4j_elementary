b396ba2601b661320013efabdd8bcf89e8a35a42
kms <do.88000@gmail.com>
2025-01-30 09:55:47 +0300

    Объявление массива [#505220]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+/**
+ *  Дополнительная литература:
+ *    а) Java 8 (или 9), Полное руководство - Герберт Шилдт. Глава 3. Массивы (стр. 94 - 101)
+ *    б) Библиотека профессионала. Java. Том 1. Основы - Кей Хостманн, Гари Корнелл.  Глава 3. Массивы (стр. 116 - 129)
+ *            Страницы примерные.
+ *
+ */
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
