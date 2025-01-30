75f3dbd7c5c39a355fc52b867095fbbad7876509
kms <do.88000@gmail.com>
2025-01-30 09:25:18 +0300

    Числовая пирамида [#505218]

---
+++ src/main/java/ru/job4j/loop/NumberPyramid.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+public class NumberPyramid {
+    public static void draw(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int i1 = n + 1 - i; i1 > 0; i1--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int j = i - 1; j > 0; j--) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Number pyramid of height 1:");
+        draw(1);
+        System.out.println("Number pyramid of height 3:");
+        draw(4);
+        System.out.println("Number pyramid of height 5:");
+        draw(5);
+    }
+}
