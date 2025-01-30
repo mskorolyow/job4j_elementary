668ae42ac3df41f8751b512854b9d8cb1ad9d34d
kms <do.88000@gmail.com>
2025-01-30 11:06:09 +0300

    Массивы и цикл for-each [#505224]

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -1,5 +1,12 @@
 package ru.job4j.array;
 
+/**
+ *  for (int i = 0; i < numbers.length; i++) {
+ *  System.out.println("Ячейка " + i + " массива names имеет значение: " + numbers[i]);
+ *  }
+ *  Меняем данный код на for-each
+ */
+
 public class ArrayLoop {
     public static void main(String[] args) {
         int[] numbers = new int[5];
@@ -7,8 +14,9 @@ public class ArrayLoop {
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i * 2 + 3;
         }
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println("Ячейка " + i + " массива names имеет значение: " + numbers[i]);
+
+        for (int number : numbers) {
+            System.out.println("Массив имеет значение: " + number);
         }
     }
 }
