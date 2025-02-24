fce52af198ba9c8c8e2537fada1c97b463cfe9a8
kms <do.88000@gmail.com>
2025-02-24 16:46:58 +0300

    Слово начинается с ... [#505233]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    /** @noinspection checkstyle:InnerAssignment*/
+    public static boolean startsWith(char[] word, char[] prefix) {
+        boolean result = true;
+        for (int i = 0; i < prefix.length; i++) {
+            if (word[i] != prefix[i]) {
+                result = false;
+                break;
+            }
+
+        }
+        return result;
+    }
+}
