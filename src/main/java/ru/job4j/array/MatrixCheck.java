403f266eca91eb958c86c3ac163ca82006b4cfde
kms <do.88000@gmail.com>
2025-02-26 15:08:22 +0300

    6.7.4. Выигрышные комбинации в сокобан [#505248]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -34,4 +34,15 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if ((board[2][2] == 'X') && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
 }
