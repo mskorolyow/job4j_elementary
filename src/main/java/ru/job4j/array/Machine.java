c71ea0c0fd0873fddb575c774052d50edeba123a
kms <do.88000@gmail.com>
2025-03-03 09:10:22 +0300

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#505250 #534393] Исправил на for each, убрал лишнюю переменную.

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -2,26 +2,46 @@ package ru.job4j.array;
 
 import java.util.Arrays;
 
+/** Before refactoring:
+ *  public static int[] change(int money, int price) {
+ *         int[] coins = {10, 5, 2, 1};
+ *         int[] result = new int[100];
+ *         int size = 0;
+ *         int totalChange = money - price;
+ *         for (int i = 0, j = 0; i < coins.length; i++) { *
+ *             while (totalChange > 0) {
+ *                 if (totalChange >= coins[i]) {
+ *                     result[j] = coins[i];
+ *                     j++;
+ *                     size++;
+ *                     totalChange = totalChange - coins[i];
+ *                 } else {
+ *                     break;
+ *                 }
+ *             }
+ *         }
+ *         return Arrays.copyOf(result, size);
+ *   }
+ */
+
 public class Machine {
     public static int[] change(int money, int price) {
         int[] coins = {10, 5, 2, 1};
         int[] result = new int[100];
         int size = 0;
-        int totalChange = money - price;
-        for (int i = 0, j = 0; i < coins.length; i++) {
-
-            while (totalChange > 0) {
-
-                if (totalChange >= coins[i]) {
-                    result[j] = coins[i];
-                    j++;
+        int iterator = 0;
+        for (int coin : coins) {
+            while (money - price > 0) {
+                if (money - price >= coin) {
+                    result[iterator] = coin;
+                    iterator++;
                     size++;
-                    totalChange = totalChange - coins[i];
+                    money -= coin;
                 } else {
                     break;
                 }
             }
         }
         return Arrays.copyOf(result, size);
-  }
+    }
 }
