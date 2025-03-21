09b95f9335ef1c3e7311e9dd42eab3a55d25ff6b
kms <do.88000@gmail.com>
2025-03-21 09:48:27 +0300

    3. Перегрузить метод max для трех чисел [#122644 #536799]

---
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxTest {
+
+    @Test
+    void whenMaxHaveOneVariables() {
+        Max max = new Max(0, 0);
+        int expected = 20;
+        int output = max.max(10, 20);
+        assertEquals(expected, output);
+
+    }
+
+    @Test
+    void whenMaxHaveTwoVariables() {
+        Max max = new Max(0, 0, 0);
+        int expected = 8;
+        int output = max.max(3, 5, 8);
+        assertEquals(expected, output);
+    }
+
+    @Test
+    void whenMaxHaveThreeVariables() {
+        Max max = new Max(0, 0, 0, 0);
+        int expected = 8;
+        int output = max.max(3, 5, 8, 7);
+        assertEquals(expected, output);
+    }
+}
